61d572f3d9c5ab517c7f9a14a3638021f21eefc0
Will C. Fisher, Jr <willcfisherjr@gmail.com>
2014-08-18 19:39:51 -0400

    Fixed brokenness regarding parameter passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit({ beverage_ids: [] }, :email, :current_password, :password, :password_confirmation, :name, :gender, :weight, :bio) }
+  end
+
 end
