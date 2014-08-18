f1cb9eefa8f3e0558c29a22149c0ce65fdb5c1a6
Will C. Fisher, Jr <willcfisherjr@gmail.com>
2014-08-18 17:57:37 -0400

    updated user class with devise authentication

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def user_login_links
+    if user_signed_in?
+      content_tag(:span, current_user.email, style: "color:green;") +
+        (link_to "Edit", edit_user_registration_path) +
+        (link_to "Log Out", destroy_user_session_path, method: :delete)
+    else
+      link_to "Log In", new_user_session_path
+    end
+  end
 end
